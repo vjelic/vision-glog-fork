1795c6e4f484b26e3ff1377bc889ef085d422493
NISHIY-EKSDEE <slava.astra@yahoo.com>
2023-02-13 00:39:11 +0100

    Update recipe

---
+++ packaging/torchvision/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ "$CONDA_BUILD_VARIANT" == rocm ]]; then
+    # remove pyproject.toml
+    rm $SRC_DIR/pyproject.toml
+
+    export ROCM_HOME=$PREFIX
+    export CMAKE_PREFIX_PATH=$PREFIX
+    export TORCHVISION_INCLUDE="${PREFIX}/include/"
+    export USE_NINJA=0
+    export FORCE_CUDA=1
+    python setup.py install
+else
+    python setup.py install --single-version-externally-managed --record=record.txt
+fi
