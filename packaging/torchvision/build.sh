37a8cd614456d44116c74c343afdab48336b30c5
Viacheslav Astrakhantsev <48382010+NISHIY-EKSDEE@users.noreply.github.com>
2023-05-31 02:58:44 +0200

    Update build.sh

--- packaging/torchvision/build.sh
+++ packaging/torchvision/build.sh
@@ -10,7 +10,7 @@ if [[ "$CONDA_BUILD_VARIANT" == rocm ]]; then
     export TORCHVISION_INCLUDE="${PREFIX}/include/"
     export USE_NINJA=0
     export FORCE_CUDA=1
-    python setup.py install
+    python setup.py install --single-version-externally-managed --record=record.txt
 else
     python setup.py install --single-version-externally-managed --record=record.txt
 fi
