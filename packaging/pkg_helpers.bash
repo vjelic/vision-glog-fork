c1ce9c9b3160986a9b9e979cc44790b5d8429676
Jithun Nair <jithun.nair@amd.com>
2023-10-06 20:38:49 +0000

    Add py3.11 support

--- packaging/pkg_helpers.bash
+++ packaging/pkg_helpers.bash
@@ -172,6 +172,7 @@ setup_wheel_python() {
       3.8) python_abi=cp38-cp38 ;;
       3.9) python_abi=cp39-cp39 ;;
       3.10) python_abi=cp310-cp310 ;;
+      3.11) python_abi=cp311-cp311 ;;
       *)
         echo "Unrecognized PYTHON_VERSION=$PYTHON_VERSION"
         exit 1
